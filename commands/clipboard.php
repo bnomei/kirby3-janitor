75f40377fee666317bfea10a5fa516c0b840ef19
Bruno Meilick <b@bnomei.com>
2022-11-10 13:55:32 +0000

    :construction: converting from jobs to commands
    Signed-off-by: bnomei <b@bnomei.com>

--- commands/clipboard.php
+++ commands/clipboard.php
@@ -7,7 +7,7 @@ return [
     'description' => 'Pipe `data` to `clipboard` arg in Janitor',
     'args' => [] + Janitor::ARGS, // page, file, user, site, data
     'command' => static function (CLI $cli): void {
-        exec('echo "'.$cli->arg('data').'" | pbcopy');
+        defined('STDOUT') && exec('echo "'.$cli->arg('data').'" | pbcopy');
         defined('STDOUT') && $cli->success('Copied "'.$cli->arg('data').'" to your clipboard.');
 
         janitor()->data($cli->arg('command'), [
